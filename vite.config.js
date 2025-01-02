97acb096ccf02a40b22d8efa2c9e7f8af5e204e2
madhavbanjade <madhavbanjade005@gmail.com>
2025-01-02 23:24:20 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-    build: {
-    outDir: 'dist', // Make sure the output directory is dist (default)
-  },
+  
   plugins: [react()],
 })
